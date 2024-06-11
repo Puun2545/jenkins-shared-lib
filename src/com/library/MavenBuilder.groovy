2ec40a0111194cb8091d0180f116aed48312436d
puun2545 <puun.vach@gmail.com>
2024-06-11 15:02:58 +0700

    fixed bug 3

--- src/com/library/MavenBuilder.groovy
+++ src/com/library/MavenBuilder.groovy
@@ -1,7 +1,7 @@
 package com.library
 
 class MavenBuilder {
-    static def build(script, String mavenVersion, String goals = 'clean package') {
+    static def build(script, String mavenVersion, String goals) {
         script.sh "${script.env.MAVEN_HOME}/bin/mvn ${goals} -Dproject.version=${mavenVersion}"
     }
 }
--- vars/examples.groovy
+++
@@ -1,3 +0,0 @@
-def call() {
-    echo org.foo.Bar.sayHello()
-}
