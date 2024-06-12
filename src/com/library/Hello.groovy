a00cc7e26de47b3435d6c69bdc24204680adb296
puun2545 <puun.vach@gmail.com>
2024-06-12 11:42:28 +0700

    versioning v0.0.2

--- src/com/library/Hello.groovy
+++ src/com/library/Hello.groovy
@@ -1,6 +1,7 @@
 package com.library
 
 class Hello {
+    // for testing purposes
     static def sayHello() {
         return "Hello, World! form library!"
     }
--- src/com/library/MavenBuilder.groovy
+++ src/com/library/MavenBuilder.groovy
@@ -1,6 +1,7 @@
 package com.library
 
 class MavenBuilder {
+    // building with Maven
     static def build(script, String mavenVersion, String goals) {
         script.sh "${script.env.MAVEN_HOME}/bin/mvn ${goals} -Dproject.version=${mavenVersion}"
     }
